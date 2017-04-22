8e2405be221ffb32771c80df0a4b7faed05a3e00
Bob Rockers <bob@rockerssoft.org>
2017-04-22 12:37:41 -0500

    Post and put

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 8088;
 const dbserver = 'mongodb://localhost/todo'
 
 var tasks = require('./routes/tasks');
+var client = require('./client');
 
 mongoose.connect(dbserver);
 var db = mongoose.connection;
@@ -18,10 +19,10 @@ db.once('open', function(){
 	console.log("Mongoose connection established.");
 });
 
-server.get('/', function(req, res, next){
-	res.send("SUCCESS");
-});
+server.get('/', client.get);
 server.get('/tasks', tasks.read);
+server.post('/tasks/:arg1', tasks.create);
+server.put('/tasks/:arg1', tasks.update);
 
 server.listen(port, function(){
 	console.log('%s listening on %s', server.name, port);
--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -7,3 +7,32 @@ exports.read = function(req, res, next){
 	});
 	return next();
 }
+
+exports.create = function(req, res, next){
+	var task = new Tasks();
+
+	task.task = req.params.arg1;
+	task.date = new Date();
+	task.completed = false;
+
+	task.save(function(err,data){
+		if(err){ console.log("Error saving to db: " + err); }
+		else { res.json( {status: "success", id: data._id} ); }
+	});
+	return next();
+}
+
+exports.update = function(req, res, next){
+	var arg = req.params.arg1;
+	var query = { task: arg };
+
+	Tasks.update( query, {completed: true}, function(err, doc){
+		if(err){ 
+			console.log("Unable to update document" + err ); 
+			res.send(404);
+		} else {
+			res.json( {status: "success"} );
+		}
+		return next();
+	});
+}
