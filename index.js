e8fceee2994499ac53b00c43378c892a503c3602
Bob Rockers <bob@rockerssoft.org>
2017-04-27 13:25:43 -0500

    Updated index file for the final parts of class.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var mongoose = require('mongoose');
 var server = restify.createServer();
+// server.use(restify.queryParser());
 
 const port = 8088;
 const dbserver = 'mongodb://localhost/todo'
@@ -23,6 +24,7 @@ server.get('/', client.get);
 server.get('/tasks', tasks.read);
 server.post('/tasks/:arg1', tasks.create);
 server.put('/tasks/:arg1', tasks.update);
+// server.post('/path/:arg1/:arg2', functionCall);
 
 server.listen(port, function(){
 	console.log('%s listening on %s', server.name, port);
